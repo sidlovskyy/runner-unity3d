c566192151a8169ea2635db27b074c599f4004a2
sidlovskyy <sidlovskyy@gmail.com>
2013-07-28 23:33:01 +0300

    Dead code removed.

--- src/Assets/Scripts/GlobalConstants.cs
+++ src/Assets/Scripts/GlobalConstants.cs
@@ -4,9 +4,5 @@ using System.Collections;
 public class GlobalConstants : MonoBehaviour
 {
 	public const float TrackSpeed = 15.0f;
-	public const float StartOfTrack = -5.0f;
-	public const float TrackWidth = 4.0f;
-	public const float ObstacleWith = 1.5f;
-	
 	public const float TerrainTileSize = 4.0f;
 }
